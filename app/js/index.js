d732421319ab63de7222ec90af99b23c47fbc884
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-05-16 13:38:05 +0800

    fix eslint tip bug

--- app/js/index.js
+++ app/js/index.js
@@ -4,28 +4,26 @@
  * @date    2016-05-16 12:15:14
  * @version $Id$
  */
+var View;
+var page;
 
-(function(){
-  'use strict';
+console.log('Hello world');
 
-  console.log('Hello world');
+View = function (ops) {
+  this.init(ops);
+};
 
-  var View = function(ops){
+View.prototype.init = function () {
+  console.log('init');
 
-    this.init(ops);
-  };
+  this.changeTitle();
+};
 
-  View.prototype.init = function(ops) {
-    console.log('init');
+View.prototype.changeTitle = function () {
+  var ele = document.getElementById('title');
+  ele.innerText = 'Hello pre-commit';
+};
 
-    this.changeTitle();
-  };
+page = new View();
 
-  View.prototype.changeTitle = function(){
-    var ele = document.getElementById('title');
-    ele.innerText = 'Hello pre-commit';
-  };
-
-  new View();
-
-})();
+console.log(page);
